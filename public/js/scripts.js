82e820fbe09070d1dfe0dd93f65640b06d74cdc6
jorgeramirezpe <jorgeramirez.pe@gmail.com>
2017-12-04 01:28:27 -0500

    Agregar Twitter

---
+++ public/js/scripts.js
@@ -0,0 +1,42 @@
+$('.button-collapse').sideNav({
+	menuWidth: "80%", // Default is 300
+	edge: 'left', // Choose the horizontal origin
+	closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+	draggable: true
+});
+$('select').material_select();
+$('.modal').modal();
+// browser window scroll (in pixels) after which the "back to top" link is shown
+var offset = 300,
+//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+offset_opacity = 1200,
+//duration of the top scrolling animation (in ms)
+scroll_top_duration = 700,
+//grab the "back to top" link
+$back_to_top = $('.cd-top');
+
+//hide or show the "back to top" link
+$(window).scroll(function(){
+	( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+	if( $(this).scrollTop() > offset_opacity ) { 
+		$back_to_top.addClass('cd-fade-out');
+	}
+});
+
+//smooth scroll to top
+$back_to_top.on('click', function(event){
+	event.preventDefault();
+	$('body,html').animate({
+		scrollTop: 0 ,
+	}, scroll_top_duration
+	);
+});
+
+$('.multiple-videos').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4
+});
+
+$('.materialboxed').materialbox();
+
